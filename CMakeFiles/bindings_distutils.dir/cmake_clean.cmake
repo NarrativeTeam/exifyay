57589d29875cd78979d2b4087688474b0c376af0
fahernandez <fahernandez@qdqmedia.com>
2014-11-07 10:07:28 +0100

    cleanup

--- CMakeFiles/bindings_distutils.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/bindings_distutils"
-  "python-timestamp"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/bindings_distutils.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
